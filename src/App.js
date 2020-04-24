d92f01fa9b0141a678f78849e40957313c941247
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-24 20:25:55 +0300

    transfer header to a separate component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
-import { Container, Box } from "@material-ui/core";
+import { Container, Box, Grid } from "@material-ui/core";
+
+import { Header } from "./components/Header";
 import { makeStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-import { MenuBlock } from "./components/Menu";
-import logo from "./img/iz.png";
-
-const blockStyles = makeStyles({
+const appStyles = makeStyles({
   buttonOpen: { position: "absolute", right: 0, top: 20 },
   block: { paddingTop: "18px" }
 });
 
 export const App = () => {
-  const styles = blockStyles();
-
+  const styles = appStyles();
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClick = event => {
@@ -27,27 +23,21 @@ export const App = () => {
   return (
     <Container maxWidth="xl">
       <Box display="flex" justifyContent="center" className={styles.block}>
-        <img
-          src={logo}
-          alt="логотип визажиста ирины жуковой"
-          height={30}
-          width={84}
+        <Header
+          handleClick={handleClick}
+          handleClose={handleClose}
+          anchorEl={anchorEl}
         />
-
-        <IconButton
-          aria-label="more"
-          aria-controls="long-menu"
-          aria-haspopup="true"
-          classes={{
-            root: styles.buttonOpen
-          }}
-          onClick={e => {
-            handleClick(e);
-          }}
-        >
-          <MoreVertIcon />
-        </IconButton>
-        <MenuBlock handleClose={handleClose} anchorEl={anchorEl} />
+        <main>
+          <div>
+            <Grid
+              container
+              direction="column"
+              justify="flex-start"
+              alignItems="center"
+            />
+          </div>
+        </main>
       </Box>
     </Container>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "../img/iz.png";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { MenuBlock } from "./Menu";
+import { makeStyles } from "@material-ui/core/styles";
+
+const headerStyles = makeStyles({
+  buttonOpen: { position: "absolute", right: 0, top: 20 },
+  block: { paddingTop: "18px" }
+});
+
+export const Header = ({handleClick, handleClose, anchorEl}) => {
+  const styles = headerStyles();
+  return (
+    <header>
+      <img
+        src={logo}
+        alt="логотип визажиста ирины жуковой"
+        height={30}
+        width={84}
+      />
+
+      <IconButton
+        aria-label="more"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        classes={{
+          root: styles.buttonOpen
+        }}
+        onClick={e => {
+          handleClick(e);
+        }}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <MenuBlock handleClose={handleClose} anchorEl={anchorEl} />
+    </header>
+  );
+};

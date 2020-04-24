12809764813677e98d7bc8397461ed0478539f9a
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-24 16:29:02 +0300

    add container and logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Container, Box } from "@material-ui/core";
+import logo from "./img/iz.png";
 
 export const App = () => {
-  return <div className="App">hello world</div>;
+  return (
+    <Container maxWidth="xl">
+      <Box>
+        <img
+          src={logo}
+          alt="логотип визажиста ирины жуковой"
+          height={30}
+          width={84}
+        />
+      </Box>
+    </Container>
+  );
 };

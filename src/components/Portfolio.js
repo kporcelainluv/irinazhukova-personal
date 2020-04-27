35936dd7c24abf9eb36458127ff58d362b5bf202
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-27 14:15:55 +0300

    add block heading, subheading

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,8 @@ import withInstagramFeed from "origen-react-instagram-feed";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
 const portfolioStyles = makeStyles({
   wrapper: {
@@ -11,27 +13,68 @@ const portfolioStyles = makeStyles({
   image: {
     width: "100%",
     height: "100%"
+  },
+  wrap: {
+    ["@media (min-width:960px)"]: {
+      flexGrow: 0,
+      maxWidth: "50%",
+      flexBasis: "50%"
+    }
+  },
+  heading: {
+    fontSize: "17px",
+    lineHeight: "24px",
+    letterSpacing: "-0.13px",
+    textTransform: "uppercase",
+    color: "#474745",
+    display: "flex",
+    justifyContent: "center"
+  },
+  subheading: {
+    color: "#474745",
+    display: "flex",
+    justifyContent: "center",
+    fontSize: "38px",
+    lineHeight: "60px",
+    letterSpacing: "-0.3px",
+    textAlign: "center"
   }
 });
 
 export const InstaGrid = withInstagramFeed(({ media, account }) => {
   const styles = portfolioStyles();
   return (
-    <Grid container className={styles.wrapper}>
-      {media &&
-        media.map(({ displayImage, id, postLink, accessibilityCaption }) => (
-          <Grid item xs={6} md={2} key={id || displayImage}>
-            <ButtonBase
-              href={postLink || `https://www.instagram.com/${account}/`}
+    <div>
+      <Typography className={styles.heading}>инстаграм</Typography>
+      <Link
+        underline={"none"}
+        className={styles.subheading}
+        href={"https://www.instagram.com/zhukovairinal/"}
+      >
+        @zhukovairinal
+      </Link>
+      <Grid container className={styles.wrapper}>
+        {media &&
+          media.map(({ displayImage, id, postLink, accessibilityCaption }) => (
+            <Grid
+              item
+              xs={6}
+              md={2}
+              key={id || displayImage}
+              className={styles.wrap}
             >
-              <img
-                src={displayImage}
-                alt={accessibilityCaption || "Instagram picture"}
-                className={styles.image}
-              />
-            </ButtonBase>
-          </Grid>
-        ))}
-    </Grid>
+              <ButtonBase
+                href={postLink || `https://www.instagram.com/${account}/`}
+              >
+                <img
+                  src={displayImage}
+                  alt={accessibilityCaption || "Instagram picture"}
+                  className={styles.image}
+                />
+              </ButtonBase>
+            </Grid>
+          ))}
+      </Grid>
+    </div>
   );
 });

d9e2de9f9cf36ea625d50b6dc46884b880c089b9
Zhukova Xenia <zhukovaksusha@gmail.com>
2021-02-28 16:14:59 +0300

    Remove more btn

--- src/components/Makeup.js
+++ src/components/Makeup.js
@@ -5,8 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 
-// import { makeup } from "../consts";
-import { makeup2 } from "../consts";
+import { makeupDesc } from "../consts";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -68,14 +67,13 @@ export const Makeup = () => {
 
   return (
     <div className={classes.root}>
-      {makeup2.map(makeup => {
-        console.log(makeup);
+      {makeupDesc.map(m => {
         return (
           <Grid container spacing={2} justify={"center"}>
             <Grid>
               <Paper className={classes.paper}>
                 <img
-                  src={makeup.imgLink}
+                  src={m.imgLink}
                   alt=""
                   height={280}
                   width={280}
@@ -86,7 +84,7 @@ export const Makeup = () => {
                   display={"block"}
                   classes={{ root: classes.lessonName }}
                 >
-                  {makeup.name}
+                  {m.name}
                 </Typography>
                 <Divider />
 
@@ -95,162 +93,12 @@ export const Makeup = () => {
                   display={"block"}
                   className={classes.lessonPrice}
                 >
-                  {makeup.price}
+                  {m.price}
                 </Typography>
-
-                <Button
-                  variant="contained"
-                  className={classes.lessonButton}
-                  onClick={() => {
-                    setOpened(!opened);
-                  }}
-                >
-                  Подробнее
-                </Button>
-
-                {opened && (
-                  <List
-                    variant="h2"
-                    display={"block"}
-                    className={classes.additionalInfo}
-                  >
-                    {makeup.description.map((element, index) => {
-                      return (
-                        <ListItem className={classes.listItem} key={element}>
-                          {element}
-                        </ListItem>
-                      );
-                    })}
-                  </List>
-                )}
               </Paper>
             </Grid>
           </Grid>
         );
       })}
     </div>
-  );
-  //
-  //   <Grid container spacing={2} justify={"center"}>
-  //     <Grid>
-  //       <Paper className={classes.paper}>
-  //         <img
-  //           src={makeup.imgLink}
-  //           alt=""
-  //           height={280}
-  //           width={280}
-  //           className={classes.img}
-  //         />
-  //         <Typography
-  //           variant="h2"
-  //           display={"block"}
-  //           classes={{ root: classes.lessonName }}
-  //         >
-  //           {makeup.name}
-  //         </Typography>
-  //         <Divider />
-  //
-  //         <Typography
-  //           variant="h4"
-  //           display={"block"}
-  //           className={classes.lessonPrice}
-  //         >
-  //           {makeup.price}
-  //         </Typography>
-  //
-  //         <Button
-  //           variant="contained"
-  //           className={classes.lessonButton}
-  //           onClick={() => {
-  //             setOpened(!opened);
-  //           }}
-  //         >
-  //           Подробнее
-  //         </Button>
-  //
-  //         {opened && (
-  //           <List
-  //             variant="h2"
-  //             display={"block"}
-  //             className={classes.additionalInfo}
-  //           >
-  //             {makeup.description.map((element, index) => {
-  //               return (
-  //                 <ListItem className={classes.listItem} key={element}>
-  //                   {element}
-  //                 </ListItem>
-  //               );
-  //             })}
-  //           </List>
-  //         )}
-  //       </Paper>
-  //     </Grid>
-  //   </Grid>
-  // </div>
-};
-
-// return (
-//   <div className={classes.root}>
-//     <Grid container spacing={2} justify={"center"}>
-//       <Grid>
-//         <Paper className={classes.paper}>
-//           <img
-//             src={makeup.imgLink}
-//             alt=""
-//             height={280}
-//             width={280}
-//             className={classes.img}
-//           />
-//           <Typography
-//             variant="h2"
-//             display={"block"}
-//             classes={{ root: classes.lessonName }}
-//           >
-//             {makeup.name}
-//           </Typography>
-//           <Divider />
-//           <Typography
-//             variant="h3"
-//             display={"block"}
-//             className={classes.lessonIntro}
-//           >
-//             {makeup.intro}
-//           </Typography>
-//           <Typography
-//             variant="h4"
-//             display={"block"}
-//             className={classes.lessonPrice}
-//           >
-//             {makeup.price}
-//           </Typography>
-//
-//           <Button
-//             variant="contained"
-//             className={classes.lessonButton}
-//             onClick={() => {
-//               setOpened(!opened);
-//             }}
-//           >
-//             Подробнее
-//           </Button>
-//
-//           {opened && (
-//             <List
-//               variant="h2"
-//               display={"block"}
-//               className={classes.additionalInfo}
-//             >
-//               {makeup.description.map((element, index) => {
-//                 return (
-//                   <ListItem className={classes.listItem} key={element}>
-//                     {element}
-//                   </ListItem>
-//                 );
-//               })}
-//             </List>
-//           )}
-//         </Paper>
-//       </Grid>
-//     </Grid>
-//   </div>
-// );
+  )};
